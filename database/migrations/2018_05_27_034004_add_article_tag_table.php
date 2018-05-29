d2cbf99d58e61d993ee41a8910888a921dbc286b
JacksonGonzalez <jaesgoca@gmail.com>
2018-05-28 21:54:24 -0500

    Commit de cambio de proyecto a blog2

---
+++ database/migrations/2018_05_27_034004_add_article_tag_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddArticleTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //tabla de pivote para la relacion muchos a muchos
+        //poner los 2 nombres en singular
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('article_id');
+            $table->unsignedInteger('tag_id');
+
+            //hacer las relaciones
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_tag');
+    }
+}
