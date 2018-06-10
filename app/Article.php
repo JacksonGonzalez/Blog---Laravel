9122f5f55ffa65f9763663509db2e80b27c938a2
JacksonGonzalez <jaesgoca@gmail.com>
2018-06-10 17:12:58 -0500

    Se añaden los packagist

--- app/Article.php
+++ app/Article.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Article extends Model
 {
+
     protected $table = 'articles';
 
     protected $fillable = ['title', 'content', 'category_id', 'user_id'];
